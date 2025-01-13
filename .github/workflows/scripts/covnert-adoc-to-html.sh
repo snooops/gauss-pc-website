382651090895454646ff94e048652735662abd07
Snooops <snooops@dns-serve.de>
2025-01-13 08:29:32 +0100

    first release

---
+++ .github/workflows/scripts/covnert-adoc-to-html.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#!/bin/bash
+set -e
+
+CURRENT_PATH=`pwd`
+ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
+
+# -a, --attribute=ATTRIBUTE
+# -B, --base-dir=DIR
+# -D, --destination-dir=DIR
+# -o, --out-file=OUT_FILE
+# -R, --source-dir=DIR
+# -b, --backend=BACKEND
+# -d, --doctype=DOCTYPE
+# -r, --require=LIBRARY
+
+# Output HTML
+mkdir -p ./outputs/html
+asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html -r asciidoctor-diagram index.adoc
+
+cp -r ${CURRENT_PATH}/src/images outputs/html
